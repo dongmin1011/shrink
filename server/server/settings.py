f66a289ba273f6184ec807aaa13e3a94fa27b45d
ad <nogo22@naver.com>
2023-12-28 00:50:58 +0900

    update: cors 설정 변경

--- server/server/settings.py
+++ server/server/settings.py
@@ -54,9 +54,9 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware'
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = (
     'http://localhost:3000',

2d603fef1ae7ee1cf13d0e108dd4b4826d10c9fb
ad <nogo22@naver.com>
2023-12-27 20:19:37 +0900

    fix: urls 수정
    - 경로 끝에 '/' 전체  추가

--- server/server/urls.py
+++ server/server/urls.py
@@ -4,11 +4,11 @@ from django.urls import path, include
 from server import views
 
 urlpatterns = [
-    path("api/favorite", include("favorite.urls")),
-    path("api/product", include("product.urls")),
+    path("api/favorite/", include("favorite.urls")),
+    path("api/product/", include("product.urls")),
     path("api/report/", include("report.urls")),
-    path("api/alert", include("alert.urls")),
-    path("api/auth", include("user_auth.urls")),
-    # path("admin", admin.site.urls),
-    path("test", views.index),
+    path("api/alert/", include("alert.urls")),
+    path("api/auth/", include("user_auth.urls")),
+    # path("admin/", admin.site.urls),
+    path("test/", views.index),
 ]

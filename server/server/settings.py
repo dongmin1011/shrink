1e42793fb8912e3948510a2d9ceb155b32a2ab5d
ad <nogo22@naver.com>
2023-12-28 00:46:03 +0900

    update: cors, csrf 허용 목록 추가

--- server/server/settings.py
+++ server/server/settings.py
@@ -61,11 +61,15 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'https://localhost:3000',
+    'https://127.0.0.1:3000',
 )
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'https://localhost:3000',
+    'https://127.0.0.1:3000',
 )
 
 

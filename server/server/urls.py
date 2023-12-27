314246a6120585b88d3c5aaf5edfffedf2bd6372
ad <nogo22@naver.com>
2023-12-27 20:52:50 +0900

    update: 질의 게시판 url 추가

--- server/server/urls.py
+++ server/server/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("api/product/", include("product.urls")),
     path("api/report/", include("report.urls")),
     path("api/alert/", include("alert.urls")),
+    path("api/query/", include("query.urls")),
     path("api/auth/", include("user_auth.urls")),
     # path("admin/", admin.site.urls),
     path("test/", views.index),

f8106cd1e9cd52d983a55fe7745258629a9533d1
dongmin <dmk4660@gmail.com>
2024-01-02 17:47:52 +0900

    신고기능 좋아요 추가

--- server/report/urls.py
+++ server/report/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
     path('delete/<int:query_id>', views.delete_report),
     path('select/detail/<int:query_id>', views.select_detail),
     path('update/<int:query_id>', views.update_report),
+    path('like/<int:query_id>', views.like_report),
+    
+    
     
     path('select/image/<str:image_url>', views.get_image),
 ]

3cdfef0ceb7098e6bc7ab8c4116740aaba273ac2
dongmin <dmk4660@gmail.com>
2024-01-05 10:32:52 +0900

    상품사진 추가

--- server/product/urls.py
+++ server/product/urls.py
@@ -8,14 +8,17 @@ urlpatterns = [
     # path('select', views.select),
     path('selectall', views.selectall),
     path('select/detail/<str:query_id>', views.select_id),
+    path('get_image/<str:url>', views.get_image ),
     
     path('analysis', views.analysis),
-    path('detect/<str:image_url>', views.get_image , name='detect_image'),
+    path('detect/<str:image_url>', views.get_analysis_image , name='detect_image'),
     path('select/priceChange', views.selectProduct),
     path('select/analysis_list', views.token_analysis_list),
     path('update/analysis', views.read_update),
     
     path('search', views.search_product),
+    
+    path('upload_image/<str:query_id>', views.upload_product_image),
     
     path('test', views.test),
     path('test2', views.test2),
